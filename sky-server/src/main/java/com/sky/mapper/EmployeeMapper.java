ae55da3f98f1d3b846f6c208a86fef235389ce05
Moirrra <moira736043695@163.com>
2024-03-17 00:34:03 +0800

    编辑员工

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -37,8 +37,17 @@ public interface EmployeeMapper {
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
+
+    /**
      * 修改员工属性
      * @param employee
      */
     void update(Employee employee);
+
 }
