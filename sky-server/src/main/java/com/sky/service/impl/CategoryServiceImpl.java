058341301914341768a40f262459c2f3ccb1e208
Moirrra <moira736043695@163.com>
2024-03-17 15:25:50 +0800

    删除分类-未与菜品关联

--- sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -82,4 +82,14 @@ public class CategoryServiceImpl implements CategoryService {
         category.setUpdateUser(BaseContext.getCurrentId());
         categoryMapper.update(category);
     }
+
+    /**
+     * 根据id删除分类
+     * @param id
+     */
+    @Override
+    public void deleteById(Long id) {
+        // TODO 和菜品的关联
+        categoryMapper.deleteById(id);
+    }
 }
