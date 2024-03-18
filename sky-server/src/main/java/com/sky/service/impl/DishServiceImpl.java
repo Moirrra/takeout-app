d68a6934b079ba71574bb627102fe98fbbe99f6a
Moirrra <moira736043695@163.com>
2024-03-18 17:25:10 +0800

    删除菜品-优化

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -87,11 +87,15 @@ public class DishServiceImpl implements DishService {
         }
 
         // 删除菜品表中的菜品
-        for (Long id : ids) {
-            dishMapper.deleteById(id);
-            // 删除菜品关联的口味
-            dishFlavorMapper.deleteByDishId(id);
-        }
+        // for (Long id : ids) {
+        //     dishMapper.deleteById(id);
+        //     // 删除菜品关联的口味
+        //     dishFlavorMapper.deleteByDishId(id);
+        // }
 
+        // 根据菜品id集合批量删除菜品
+        dishMapper.deleteByIds(ids);
+        // 根据菜品id集合批量删除口味
+        dishFlavorMapper.deleteByDishIds(ids);
     }
 }
